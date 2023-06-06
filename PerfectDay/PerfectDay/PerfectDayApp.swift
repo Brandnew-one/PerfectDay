da0d8588d78f01bf6cc1395ccb04fcb411aca1d0
Bran <cold929@naver.com>
2023-06-06 22:07:42 +0900

    chore: project init

---
+++ PerfectDay/PerfectDay/PerfectDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectDayApp.swift
+//  PerfectDay
+//
+//  Created by Bran on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct PerfectDayApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
