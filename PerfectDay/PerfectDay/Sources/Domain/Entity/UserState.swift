9209a066c0f60b7e7fd0fb91454e458dee5f72b9
Bran <cold929@naver.com>
2023-06-30 01:18:40 +0900

    feat: notification repository
    - 알림 권한 설정을 위한 Repository 구현

---
+++ PerfectDay/PerfectDay/Sources/Domain/Entity/UserState.swift
@@ -0,0 +1,14 @@
+//
+//  UserState.swift
+//  PerfectDay
+//
+//  Created by Bran on 2023/06/28.
+//  Copyright © 2023 sangwon. All rights reserved.
+//
+
+import Foundation
+
+enum UserState {
+  case approved
+  case denied
+}
